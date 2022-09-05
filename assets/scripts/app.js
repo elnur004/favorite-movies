4962116981ddfbba9ec3c0227c264012820bc6d5
elnur004 <lnur004@gmail.com>
2022-09-06 01:03:22 +0400

    fixed element and handler function names

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 const startAddMovieButton = document.querySelector('header button');
-const showMovie = document.getElementById('add-modal');
+const popupMovieModal = document.getElementById('add-modal');
 const modal = document.querySelector('.modal');
 const backdropElement = document.getElementById('backdrop');
-const cancelButton = document.querySelector('.btn--passive');
+const cancelButton = popupMovieModal.querySelector('.btn--passive');
 
 const backdropHandler = () => {
   backdropElement.classList.toggle('visible');
@@ -13,11 +13,11 @@ const removeBackdropHandler = () => {
   backdropElement.classList.remove('visible');
 };
 
-const showMovieHandler = () => {
-  showMovie.classList.toggle('visible');
+const popupMovieModalHandler = () => {
+  popupMovieModal.classList.toggle('visible');
   backdropHandler();
 };
 
 backdropElement.addEventListener('click', removeBackdropHandler);
 cancelButton.addEventListener('click', removeBackdropHandler);
-startAddMovieButton.addEventListener('click', showMovieHandler);
+startAddMovieButton.addEventListener('click', popupMovieModalHandler);
