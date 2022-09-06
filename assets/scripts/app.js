d35c759c91876e5c2b391c3dbf3632a02528304b
elnur004 <lnur004@gmail.com>
2022-09-07 02:48:29 +0400

    added DOM elements and eventHandler and fixed Handler function name

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,29 @@
 const startAddMovieButton = document.querySelector('header button');
 const popupMovieModal = document.getElementById('add-modal');
-const modal = document.querySelector('.modal');
 const backdropElement = document.getElementById('backdrop');
 const cancelButton = popupMovieModal.querySelector('.btn--passive');
+//const addButton = cancelButton.nextElementSibling; //DOM traversal technique
+const addButton = document.querySelector('.btn--success');
 
 const backdropHandler = () => {
   backdropElement.classList.toggle('visible');
 };
 
 const removeBackdropHandler = () => {
-  modal.classList.remove('visible');
+  popupMovieModal.classList.remove('visible');
   backdropElement.classList.remove('visible');
 };
 
-const popupMovieModalHandler = () => {
+const cancelMovieModalHandler = () => {
   popupMovieModal.classList.toggle('visible');
   backdropHandler();
 };
 
+const addMovieHandler = () => {
+  console.log('Movie added!');
+};
+
 backdropElement.addEventListener('click', removeBackdropHandler);
 cancelButton.addEventListener('click', removeBackdropHandler);
-startAddMovieButton.addEventListener('click', popupMovieModalHandler);
+addButton.addEventListener('click', addMovieHandler);
+startAddMovieButton.addEventListener('click', cancelMovieModalHandler);
