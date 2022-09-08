df943c76719d3a7ed61185860268e19994ca5ff5
elnur004 <lnur004@gmail.com>
2022-09-08 04:31:40 +0400

    added another way to fetch and validate data from UI

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,9 +4,10 @@ const backdropElement = document.getElementById('backdrop');
 const cancelButton = popupMovieModal.querySelector('.btn--passive');
 //const addButton = cancelButton.nextElementSibling; //DOM traversal technique
 const addButton = document.querySelector('.btn--success');
-const movieTitle = document.getElementById('title');
-const movieUrl = document.getElementById('image-url');
-const movieRating = document.getElementById('rating');
+// const movieTitle = document.getElementById('title');
+// const movieUrl = document.getElementById('image-url');
+// const movieRating = document.getElementById('rating');
+const userInputs = popupMovieModal.querySelectorAll('input');
 const movieListEl = document.getElementById('movie-list');
 
 const backdropToggle = () => {
@@ -18,10 +19,16 @@ const removeBackdropHandler = () => {
   backdropElement.classList.remove('visible');
 };
 
+// const clearEnteredValues = () => {
+//   movieTitle.value = '';
+//   movieUrl.value = '';
+//   movieRating.value = '';
+// };
+
 const clearEnteredValues = () => {
-  movieTitle.value = '';
-  movieUrl.value = '';
-  movieRating.value = '';
+  userInputs[0].value = '';
+  userInputs[1].value = '';
+  userInputs[2].value = '';
 };
 
 const cancelMovieModalHandler = () => {
@@ -30,21 +37,46 @@ const cancelMovieModalHandler = () => {
   clearEnteredValues();
 };
 
-const movieList = (title, url, rating) => {
-  if (title && url && rating) {
-    movieListEl.insertAdjacentHTML(
-      'beforeend',
-      `<li>${title}</li> <li>${url}</li> <li>${+rating}</li>`
-    );
-    removeBackdropHandler();
-    clearEnteredValues();
-  } else if (!title || !url || !+rating) {
-    alert(`Enter valid title or url or rating`);
-  }
-};
+// const movieList = (title, url, rating) => {
+//   if (title && url && rating) {
+//     movieListEl.insertAdjacentHTML(
+//       'beforeend',
+//       `<li>${title}</li> <li>${url}</li> <li>${+rating}</li>`
+//     );
+//     removeBackdropHandler();
+//     clearEnteredValues();
+//   } else if (!title || !url || !+rating) {
+//     alert(`Enter valid title or url or rating`);
+//   }
+// };
 
 const addMovieHandler = () => {
-  movieList(movieTitle.value, movieUrl.value, movieRating.value);
+  // movieList(movieTitle.value, movieUrl.value, movieRating.value);
+  const movieTitle = userInputs[0].value;
+  const movieUrl = userInputs[1].value;
+  const movieRating = +userInputs[2].value;
+
+  if (
+    movieTitle.trim() &&
+    movieUrl.trim() &&
+    +movieRating >= 1 &&
+    +movieRating <= 5
+  ) {
+    movieListEl.insertAdjacentHTML(
+      'beforeend',
+      `<li>${movieTitle}</li> <li>${movieUrl}</li> <li>${+movieRating}</li>`
+    );
+    removeBackdropHandler();
+    clearEnteredValues(movieTitle, movieUrl, movieRating);
+  } else if (
+    !movieTitle.trim() ||
+    !movieUrl.trim() ||
+    !+movieRating ||
+    +movieRating < 1 ||
+    +movieRating > 5
+  ) {
+    alert(`Enter either valid title, url or rating between 1 and 5`);
+  }
 };
 
 backdropElement.addEventListener('click', removeBackdropHandler);
